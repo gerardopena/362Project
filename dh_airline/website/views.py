f67016182da026c302644d81f2fdff24631d728b
Brandon Le <le.brandon350@gmail.com>
2020-04-09 14:21:44 -0700

    more updates

--- dh_airline/website/views.py
+++ dh_airline/website/views.py
@@ -5,7 +5,7 @@ from flights.models import Flight
 # Create your views here.
 
 class IndexView(TemplateView):
-    template_name='base.html'
+    template_name='homepage.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
